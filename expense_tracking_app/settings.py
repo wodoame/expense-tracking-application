d5b0ad6a1f7d87ffc6e2b9b2af495578f3b22578
Wodoame <bmwodoame@gmail.com>
2024-09-24 10:49:54 +0000

    FIX render deployment configurations

--- expense_tracking_app/settings.py
+++ expense_tracking_app/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r%ww*=9kb2(n(jt&b40ufb&hr^g3%*ex)(v#w*6=d($y+)&cwp'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
