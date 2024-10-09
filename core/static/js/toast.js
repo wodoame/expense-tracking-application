c7becb513f17a1f53b05f3086e64c83e83f8a2d2
Wodoame <bmwodoame@gmail.com>
2024-10-09 22:52:41 +0000

    MODIFY toast component and favicon

--- core/static/js/toast.js
+++ core/static/js/toast.js
@@ -2,12 +2,8 @@
 (function () {
     document.addEventListener('alpine:init', () => {
         Alpine.data('toast', () => ({
-            open: false,
-            // close(){
-            //   setTimeout(()=>{this.open = false; localStorage.setItem('toastAppeared', 'true')}, 2000);
-            // }, 
+            open: true,
             show() {
-                this.open = true;
                 setTimeout(() => { this.open = false; }, 2000);
             }
         }));
