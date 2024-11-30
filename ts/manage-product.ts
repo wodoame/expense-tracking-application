7aad762dcee71373a805197c9119177ec5ad5b3c
Wodoame <bmwodoame@gmail.com>
2024-11-30 13:23:38 +0000

    refactor: change how a product is accessed

--- ts/manage-product.ts
+++ ts/manage-product.ts
@@ -1,24 +1,10 @@
-const products: Product[] = JSON.parse(document.getElementById('products').textContent); 
-
-const getProduct = (id: string)=>{
-    for(const product of products){
-        if(product.id == +id){
-        //  console.log(product);
-            return product;
-        }
-    }
-};
-    
-// instead of the function below if it becomes necessay (eg. if the UI needs to be dynamic ) I will use string literals to generate html and insert them 
-// into the DOM using .insertAdjacentHTML() 
-const setDetail = (modal:HTMLElement, query:string, value: string)=>{
+ const setDetail = (modal:HTMLElement, query:string, value: string)=>{
     modal.querySelector(query).textContent = value;
 };
 
 const showDetails = (e: Event)=>{
     e.stopPropagation();
-    const productId = (<HTMLElement>e.currentTarget).id;
-    const product = getProduct(productId);
+    const product: Product = JSON.parse((<HTMLElement>e.currentTarget).dataset.info); 
     const id = 'show-details-modal';
     const modal = document.getElementById(id);
     setDetail(modal, `#${id}-product-name`, product.name); 
@@ -30,8 +16,7 @@ const showDetails = (e: Event)=>{
 
 const editProduct = (e: Event)=>{
    e.stopPropagation();
-   const productId = (<HTMLElement>(<HTMLElement>e.currentTarget).parentNode.parentNode.parentNode).id;
-   const product = getProduct(productId);
+   const product: Product = JSON.parse((<HTMLElement>(<HTMLElement>e.currentTarget).parentNode.parentNode.parentNode).dataset.info);
    const modal = document.getElementById('edit-product-modal');
    const nameInput = <HTMLInputElement>modal.querySelector('[name=name]'); 
    const cedisInput = <HTMLInputElement>modal.querySelector('[name=cedis]'); 
@@ -57,14 +42,13 @@ const editProduct = (e: Event)=>{
 
 const deleteProduct = (e: Event)=>{
     e.stopPropagation();
-    const productId = (<HTMLElement>(<HTMLElement>e.currentTarget).parentNode.parentNode.parentNode).id;
-    const product = getProduct(productId);
+    const product: Product = JSON.parse((<HTMLElement>(<HTMLElement>e.currentTarget).parentNode.parentNode.parentNode).dataset.info);
     const id = 'delete-product-modal';
     const modal = document.getElementById(id);
     setDetail(modal, `#${id}-product-name`, product.name);
     setDetail(modal, `#${id}-product-price`, `GHS ${product.price.toFixed(2)}`); 
     setDetail(modal, `#${id}-product-category`, product.category? product.category.name: 'None'); 
     setDetail(modal, `#${id}-product-description`, product.description || 'No description');
-    (<HTMLInputElement>modal.querySelector('#delete-product-id')).value = productId;
+    (<HTMLInputElement>modal.querySelector('#delete-product-id')).value = product.id.toString();
     toggle(e, 'delete-product-modal') 
 }; 
