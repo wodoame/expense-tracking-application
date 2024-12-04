af1339559c363d4985e8ca2480f1360464322030
Wodoame <bmwodoame@gmail.com>
2024-12-04 16:12:09 +0000

    fix(UI): change background color of the loading skeletons for records

--- core/utils.py
+++ core/utils.py
@@ -2,8 +2,6 @@ from .models import Product
 from .serializers import ProductSerializer
 from .datechecker import get_total
 import asyncio 
-def intentionalDelay(seconds): 
-    asyncio.sleep(seconds)
 
 def record(date):
     products = ProductSerializer(Product.objects.filter(date__date=date), many=True).data
@@ -13,6 +11,6 @@ def record(date):
         'total':get_total(products)
     }
     
-    # intentionalDelay(2)
+    # asyncio.run(asyncio.sleep(2))
     return result
 
