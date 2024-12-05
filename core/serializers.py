5beff4beaaf72b713c7278a01680fd60b9aa0e98
Wodoame <bmwodoame@gmail.com>
2024-12-05 11:26:45 +0000

    feat: display number of items in each category on categories page

--- core/serializers.py
+++ core/serializers.py
@@ -2,9 +2,14 @@ from rest_framework import serializers
 from .models import Product, Category
 
 class CategorySerializer(serializers.ModelSerializer):
+    product_count = serializers.SerializerMethodField()
     class Meta: 
         model = Category
-        fields = '__all__'
+        fields = ['id', 'name', 'product_count']
+        
+    def get_product_count(self, obj):
+        return obj.products.count()
+    
 class ProductSerializer(serializers.ModelSerializer): 
     category = CategorySerializer() 
     class Meta: 
