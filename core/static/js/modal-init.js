f7b1597a26343e527e8f786df1b839b78b91ac90
Wodoame <bmwodoame@gmail.com>
2024-12-22 04:59:01 +0000

    feature: enable modals (developed with the baseModal approach) to close when the back button of the browser is pressed

--- core/static/js/modal-init.js
+++ core/static/js/modal-init.js
@@ -2,6 +2,7 @@
 class ModalManager {
     constructor() {
         this.modals = {}; // a store of created modals
+        this.currentlyOpenModal = null;
     }
     createModal(id, modalInstance) {
         this.modals[id] = modalInstance;
@@ -11,9 +12,11 @@ class ModalManager {
     }
 }
 const modalManager = new ModalManager();
-document.addEventListener('alpine:init', () => {
-    // this is where the actual creation of a modal instance takes place
-    Alpine.data('baseModal', (id) => ({
+function handleAlpineInitialization() {
+    createModal();
+}
+function createModalInstance(id) {
+    return {
         isOpen: false,
         init() {
             modalManager.createModal(id, this);
@@ -25,11 +28,36 @@ document.addEventListener('alpine:init', () => {
             const body = document.body;
             animatedBackdrop.classList.toggle('hidden');
             body.classList.toggle('overflow-hidden');
+            if (this.isOpen) {
+                history.pushState(null, '');
+                localStorage.setItem('modalOpen', 'true');
+                modalManager.currentlyOpenModal = this;
+            }
         },
         handleClickOutside(e) {
             if (e.target == e.currentTarget) {
                 this.toggle();
             }
         }
-    }));
+    };
+}
+function createModal() {
+    Alpine.data('baseModal', createModalInstance);
+}
+function handleCloseModal() {
+    if (localStorage.getItem('modalOpen')) {
+        localStorage.removeItem('modalOpen');
+        localStorage.setItem('forwarded', ' true');
+        history.forward();
+    }
+    else if (localStorage.getItem('forwarded')) {
+        localStorage.removeItem('forwarded');
+        modalManager.currentlyOpenModal.toggle();
+    }
+}
+window.addEventListener('popstate', handleCloseModal);
+document.addEventListener('alpine:init', handleAlpineInitialization);
+window.addEventListener('beforeunload', () => {
+    document.removeEventListener('alpine:init', handleAlpineInitialization);
+    window.removeEventListener('popstate', handleCloseModal);
 });
--- ts/modal-init.ts
+++ ts/modal-init.ts
@@ -1,7 +1,9 @@
 class ModalManager{
     modals:any; 
+    currentlyOpenModal: ModalInstance | null;  // track the currently opened modal
     constructor(){
         this.modals = {}; // a store of created modals
+        this.currentlyOpenModal = null;
     }
 
     createModal(id: string, modalInstance: ModalInstance){
@@ -11,13 +13,17 @@ class ModalManager{
     getModal(id: string){
         return this.modals[id];
     }
+
+
 }
 
 const modalManager = new ModalManager();
+function handleAlpineInitialization(){
+    createModal();
+}
 
-document.addEventListener('alpine:init', ()=>{
-    // this is where the actual creation of a modal instance takes place
-    Alpine.data('baseModal', (id: string)=>({
+function createModalInstance(id: string){
+    return {
         isOpen: false,
         init(){
             modalManager.createModal(id, this);
@@ -29,11 +35,39 @@ document.addEventListener('alpine:init', ()=>{
             const body = document.body;
             animatedBackdrop.classList.toggle('hidden');
             body.classList.toggle('overflow-hidden')
+            if(this.isOpen){
+                history.pushState(null, '');
+                localStorage.setItem('modalOpen', 'true');
+                modalManager.currentlyOpenModal = this;
+            }
         }, 
         handleClickOutside(e: Event){
             if(e.target == e.currentTarget){
                 this.toggle();
             }
         }
-    }))
-});
+    }
+}
+
+function createModal(){
+    Alpine.data('baseModal', createModalInstance);
+}
+
+function handleCloseModal(){
+    if(localStorage.getItem('modalOpen')){
+        localStorage.removeItem('modalOpen')
+        localStorage.setItem('forwarded',' true')
+        history.forward();
+    }
+    else if(localStorage.getItem('forwarded')){
+        localStorage.removeItem('forwarded');
+        modalManager.currentlyOpenModal.toggle();
+    }
+}
+
+window.addEventListener('popstate', handleCloseModal);
+document.addEventListener('alpine:init', handleAlpineInitialization);
+window.addEventListener('beforeunload', ()=>{
+    document.removeEventListener('alpine:init', handleAlpineInitialization);  
+    window.removeEventListener('popstate', handleCloseModal);
+})
