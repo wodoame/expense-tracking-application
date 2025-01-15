0f1f4cf40888c12b5e9e70f5e898fcd3e8204fa6
Wodoame <bmwodoame@gmail.com>
2025-01-15 11:34:50 +0000

    fix: limit number of characters for product name and category name to 25

--- core/models.py
+++ core/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from authentication.models import User
 class Category(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='categories', null=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=25)
     
     def __str__(self):
         return self.name
     
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='products', null=True)
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=25)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='products', null=True, blank=True)
     price = models.FloatField()
     date = models.DateTimeField(auto_now_add=True)
