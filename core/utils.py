cdadd763426c43bfb5334b911ace20becf655eed
Wodoame <bmwodoame@gmail.com>
2025-02-27 21:30:25 +0000

    feat: add an event emitter to run some functions when some events are triggered

--- core/utils.py
+++ core/utils.py
@@ -4,6 +4,7 @@ from .datechecker import get_total, datefromisoformat
 import asyncio 
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required as lr 
+from django.core.cache import cache
 
 def record(date, request):
     user = request.user
@@ -50,3 +51,40 @@ def getCategoriesSkeletonContext():
 def encryptAllProducts():
     for product in Product.objects.all(): 
         product.save() # saving indirectly encrypts the products
+
+
+class EventEmitter:
+    def __init__(self):
+        self._events = {}
+
+    def on(self, event_name, callback):
+        """Register an event listener for a specific event."""
+        if event_name not in self._events:
+            self._events[event_name] = []
+        self._events[event_name].append(callback)
+
+    def off(self, event_name, callback):
+        """Remove an event listener for a specific event."""
+        if event_name in self._events:
+            self._events[event_name] = [cb for cb in self._events[event_name] if cb != callback]
+
+    def emit(self, event_name, *args, **kwargs):
+        """Trigger an event and call all registered listeners."""
+        if event_name in self._events:
+            for callback in self._events[event_name]:
+                callback(*args, **kwargs)
+
+    def once(self, event_name, callback):
+        """Register a one-time event listener for a specific event."""
+        def wrapper(*args, **kwargs):
+            callback(*args, **kwargs)
+            self.off(event_name, wrapper)
+        self.on(event_name, wrapper)
+        
+
+def deleteQuickStatsFromCache(request):
+    cache.delete(f'weekly-stats-{request.user.username}')
+    cache.delete(f'monthly-stats-{request.user.username}')
+
+emitter = EventEmitter() # global event emitter
+emitter.on('products_updated', deleteQuickStatsFromCache)
