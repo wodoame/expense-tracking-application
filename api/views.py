9d781b742d2652bf837c1660efdb5b8d2588fa93
Wodoame <bmwodoame@gmail.com>
2025-03-07 02:30:37 +0000

    feat: filter search results by user id

--- api/views.py
+++ api/views.py
@@ -5,8 +5,10 @@ from rest_framework.request import Request
 from core.models import *
 from django.db.models import Q
 from whoosh import index
-from whoosh.fields import Schema, TEXT, STORED
-from whoosh.qparser import MultifieldParser, OrGroup, FuzzyTermPlugin
+from whoosh.fields import Schema, TEXT, ID, STORED
+from whoosh.qparser import MultifieldParser, OrGroup, FuzzyTermPlugin, QueryParser
+import os
+from .utils import * 
 
 class Categories(APIView):
     def get(self, request):
@@ -24,7 +26,7 @@ class Search(APIView):
         query = request.query_params.get('query').strip()
         user = request.user
         products = ProductSerializer(user.products.all(), many=True).data
-        data = self.search_products(query, products)
+        data = self.search_products(query, products, user.id)
         return Response(data)
     
     def search_categories(self, query: str) -> dict:
@@ -38,20 +40,23 @@ class Search(APIView):
         }
         return data
     
-    def search_products(self, query:str, products) -> dict:
+    def search_products(self, query:str, products, user_id) -> dict:
         # Define the schema
-        schema = Schema(id=STORED, name=TEXT(stored=True), description=TEXT(stored=True))
+        schema = Schema(id=STORED, doc_id=ID(unique=True), user_id=ID,  name=TEXT(stored=True), description=TEXT(stored=True))
 
-        # Create an in-memory index
-        # ix = index.open_dir('indexdir')
-        ix = index.create_in('indexdir', schema)
+        # Create or open the index
+        index_dir = 'indexdir'
+        if not index.exists_in(index_dir, indexname='products'):
+            os.makedirs(index_dir, exist_ok=True)
+            ix = index.create_in(index_dir, schema, indexname='products')
+        else:
+            ix = index.open_dir(index_dir, indexname='products')
 
         # Add documents to the index
         writer = ix.writer()
         for product in products:
-            writer.add_document(id=product.get('id'), name=product.get('name'), description=product.get('description'))
+            writer.update_document(id=product.get('id'), doc_id=str(product.get('id')), user_id=str(product.get('user')), name=product.get('name'), description=product.get('description'))
         writer.commit()
-        
         with ix.searcher() as searcher:
             qp = MultifieldParser(['name', 'description'], ix.schema, group=OrGroup)
             
@@ -61,6 +66,7 @@ class Search(APIView):
                 q = qp.parse(f'{query}~') # edit distance is 1
             else:
                 q = qp.parse(f'{query}')
+            q = q & QueryParser('user_id', ix.schema).parse(str(user_id))
             print(q)
             results = searcher.search(q)
             data = {
@@ -70,3 +76,16 @@ class Search(APIView):
                 'results': [dict(result) for result in results]
             }
             return data
+        
+class RecreateIndexes(APIView):
+    def get(self, request):
+        lines = read_flag()
+        newlines = []
+        indexname, value = lines[0].split(':')
+        if int(value) == 1:
+            pass
+        
+        newlines.append(f'{indexname}:0') # set value to zero
+        
+            
+        return Response({'message': 'index recreated'})
