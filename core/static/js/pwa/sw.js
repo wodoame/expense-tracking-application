2ec123c46a4b3d8c3aef7c59434027b1f2702f61
Wodoame <bmwodoame@gmail.com>
2025-05-12 21:58:02 +0000

    feat: make app a PWA

---
+++ core/static/js/pwa/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'django-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/css/output.css', // Replace with your CSS file path
+//   '/static/js/main.js',    // Replace with your JS file path
+  '/static/js/alpine.min.js',    // Replace with your JS file path
+  '/static/js/htmx.min.js',    // Replace with your JS file path
+
+//   '/static/pwa/icons/icon-192x192.png',
+  '/static/imgs/logo-100x100.png'
+];
+
+// Install the service worker and cache assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate the service worker and clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+// Fetch event to serve cached content or fetch from network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
