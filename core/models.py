ff6c1038420fea68b31e76b74da1288c6ee6385b
Wodoame <bmwodoame@gmail.com>
2025-06-06 11:55:44 +0000

    feat: add field populated_monthly_spending to settings model

--- core/models.py
+++ core/models.py
@@ -47,6 +47,7 @@ class Product(models.Model):
 class Settings(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='settings')
     populated_weekly_spending = models.BooleanField(default=False) 
+    populated_monthly_spending = models.BooleanField(default=False)
     
     def __str__(self):
         return self.user.username + ' settings'
