a0e4b2812902dfbdd251b849db143d8008318639
Wodoame <bmwodoame@gmail.com>
2025-07-29 19:39:54 +0000

    feat: add profile_picture field to user model

--- authentication/models.py
+++ authentication/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     '''the user model'''
     email = models.EmailField(unique=True)
-    
+    profile_picture = models.URLField(blank=True, null=True)
+
     def __str__(self):
         return self.username
     
