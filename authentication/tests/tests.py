6fd4485e9f704aaa85518b1722ff1a6e959c74a6
Wodoame <bmwodoame@gmail.com>
2025-07-30 03:11:40 +0000

    test: test if a user is signed up if they signin with google and their account doesn't exist

--- authentication/tests/tests.py
+++ authentication/tests/tests.py
@@ -1,11 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from authentication.views import AuthCallback
 from authentication.models import User
 from authentication.schemas import UserModel, UserMetadata, AppMetadata
-
+from unittest.mock import patch, MagicMock
+from rest_framework import status
 class CreateUserFromGoogleTest(TestCase):
     def setUp(self):
         self.auth_callback = AuthCallback()
+        self.mocked_request = MagicMock()
+        self.client = Client()
         self.user_data = UserModel(
             id='18b94c1e-21ed-42de-9605-dc1e617ad5c7',
             aud='authenticated',
@@ -41,3 +44,12 @@ class CreateUserFromGoogleTest(TestCase):
         self.assertEqual(user.first_name, 'Test User')
         self.assertEqual(user.username, 'testuser_google')
         self.assertFalse(user.has_usable_password())  # User created by oauth should have no usable password
+    
+    def test_signup_user_with_google_if_not_exists(self):
+        data = {
+            'user': self.user_data.model_dump()
+        }
+        response = self.client.post('/auth/callback/?action=signin', data=data, content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(User.objects.filter(email=self.user_data.email).exists())
+        self.assertTrue(User.objects.filter(username='testuser_google').exists())
