66fc3ae55a1216d00aec9123d61e4cbcd00d7df2
wodoame <bmwodoame@gmail.com>
2025-11-04 11:37:12 +0000

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,17 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig(({ command }) => ({
-  root: "frontend",
-  base: command === "build" ? "/static/frontend/" : "/",
-  plugins: [react()],
+export default defineConfig({
   build: {
-    outDir: "dist",
-    emptyOutDir: true,
+    outDir: 'core/static/js/vite', 
     rollupOptions: {
       output: {
-        entryFileNames: "bundle.js",
+        entryFileNames: 'bundle.js', 
         manualChunks: {
-          "vendor-large": ["apexcharts", "vanilla-calendar-pro"],
-          "vendor-ui": ["alpinejs", "flowbite", "lit"],
+          'vendor-large': ['apexcharts', 'vanilla-calendar-pro'], // Large libraries
+          'vendor-ui': ['alpinejs', 'flowbite', 'lit'], // UI libraries
         }
       }
     }
-  },
-  cacheDir: ".vite"
-}));
+  }, 
+  cacheDir: '.vite'
+});
